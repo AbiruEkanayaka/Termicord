e8d9a6a140f99d8f5859ca48437f4fe34305ed85
Abiru Ekanayaka <123209651+AbiruEkanayaka@users.noreply.github.com>
2024-04-09 11:16:06 +0000

    Update example.config.py

--- example.config.py
+++ example.config.py
@@ -1,1 +1,9 @@
-TOKEN = "Your Discord bot token goes here."
+# Discord
+TOKEN = "bot token"
+
+# PostgreSQL
+DB_Name = "db name"
+DB_User = "db username"
+DB_Pass = "db password"
+DB_Host = "host ip"
+DB_Port = 00000 # Must be an int
